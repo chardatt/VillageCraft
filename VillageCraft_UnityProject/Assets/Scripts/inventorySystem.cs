46a1c7f55df1a0ee89c3a46237c561599afcb4a9
totopo667 <lyalexiswei@hotmail.com>
2022-06-15 01:57:46 +0200

    premier inventaire
    apparition disparition de l'inventaire

---
+++ VillageCraft_UnityProject/Assets/Scripts/inventorySystem.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inventorySystem : MonoBehaviour
+{
+    public GameObject inventory;
+    bool activeInventory = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // faire apparaitre et disparaitre l'inventaire
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            /*if(activeInventory == false)
+            {
+                activeInventory = true;
+            }
+            else if (activeInventory == true)
+            {
+                activeInventory = false;
+            }*/
+            activeInventory = !activeInventory;
+            inventory.SetActive(activeInventory);
+        }
+    }
+}
