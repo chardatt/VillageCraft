812e41f2ee5e440a26fee180bf4933bbf2136166
totopo667 <lyalexiswei@hotmail.com>
2022-06-16 20:58:02 +0200

    inventory
    + inventaire + UI
    + ramasser les items
    + item ont des images à leurs noms
    + stockage d'item dans un container

--- VillageCraft_UnityProject/Assets/Scripts/Inventory scripts/PlayerInventory.cs
+++ VillageCraft_UnityProject/Assets/Scripts/Inventory scripts/PlayerInventory.cs
@@ -16,4 +16,8 @@ public class PlayerInventory : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear(); 
+    }
 }
